2989c72618f57ba1b3e7c1c2f2b9482993b78731
Roman <rombom24@gmail.com>
2022-07-20 20:52:32 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,37 @@
+window.addEventListener('load', function(){
+
+    let inp1 = document.querySelector('.inp1');
+    let inp2 = document.querySelector('.inp2');
+    let btn = document.querySelector('.go');
+    let res = document.querySelector('.res');
+
+    inp1.addEventListener('keydown', function(){
+       btn.disabled=false})
+    inp2.addEventListener('keydown', function(){
+       btn.disabled=false})
+
+    function checkNumbers(){
+        if (event.key=='Backspace') {
+            inp2.value.pop()
+        } else if(isNaN(event.key)|| event.key==' ') {
+            event.preventDefault()
+        }
+    }
+    
+    inp1.addEventListener('keydown', checkNumbers)
+    inp2.addEventListener('keydown', checkNumbers)
+
+    btn.addEventListener('click', function(){
+        if(select.value==='+'){
+            res.innerHTML = (parseInt(inp1.value) + parseInt(inp2.value))
+        } else if (select.value==='-'){
+            res.innerHTML = (parseInt(inp1.value) - parseInt(inp2.value))
+        } else if (select.value==='*'){
+            res.innerHTML = (parseInt(inp1.value) * parseInt(inp2.value))
+        } else if (select.value==='/'){
+            res.innerHTML = (parseInt(inp1.value) / parseInt(inp2.value))
+        }
+        btn.disabled=true
+    });
+});
+
