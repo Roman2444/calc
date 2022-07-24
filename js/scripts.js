0fb7fbde66e8fefb5a97106d6739c4ffcc766c5d
Roman <rombom24@gmail.com>
2022-07-24 18:05:47 +0300

    clear html, event input with bubbels, add events keydown

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,35 @@
 window.addEventListener('load', function(){
 
+    let container = document.querySelector('.container');
     let inp1 = document.querySelector('.inp1');
     let inp2 = document.querySelector('.inp2');
     let btn = document.querySelector('.go');
     let res = document.querySelector('.res');
     let oper = document.querySelector('.operation')
 
-    inp1.addEventListener('input', btnEnable)
-    inp2.addEventListener('input', btnEnable)
-    oper.addEventListener('input', btnEnable)
-    
-    function btnEnable(){
+    container.addEventListener('input', function(){
         btn.disabled = false;  
-    }
-    
-    // возможна более краткая запись
-    // (работает почему то только после function btnEnable??? мб цикл forEach не дает дойти до функции )
-    // [inp1, inp2, oper].forEach(elem => elem.addEventListener('input', btnEnable));
+    })
 
+    this.document.addEventListener('keydown', function(e) {
+        if (e.key === '+'){
+            oper.value='+'
+        } else if (e.key === '-'){
+            oper.value='-'
+        } else if (e.key === '*'){
+            oper.value='*'
+        } else if (e.key === '/'){
+            oper.value='/'
+        } 
+    })
+    
     function checkNumbers(){
         this.value = this.value.replace(/[^0-9]/g,"")
     }
     inp1.addEventListener('input', checkNumbers)
     inp2.addEventListener('input', checkNumbers)
 
+    let calculate = 
     btn.addEventListener('click', function(){
         let num1 = parseInt(inp1.value);
         let num2 = parseInt(inp2.value);
