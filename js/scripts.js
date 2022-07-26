cc2a71f43fcfd441b0b3b3eba490537e7ef19b81
Roman <rombom24@gmail.com>
2022-07-26 11:53:58 +0300

    add event push Enter, add operations object

--- js/scripts.js
+++ js/scripts.js
@@ -7,12 +7,34 @@ window.addEventListener('load', function(){
     let res = document.querySelector('.res');
     let oper = document.querySelector('.operation')
 
+    const operations = {
+        '+': (num1, num2) => num1 + num2,
+        '-': (num1, num2) => num1 - num2,
+        '*': (num1, num2) => num1 * num2,
+        '/': (num1, num2) => num1 / num2,
+    };
+    
+    function calculate(){
+        let num1 = parseInt(inp1.value);
+        let num2 = parseInt(inp2.value);
+        let result = operations[oper.value](num1, num2);
+
+        if (isNaN(result)){
+            result='Ошибка'
+        }
+        res.innerHTML = result 
+        btn.disabled = true
+    }
+
+    btn.addEventListener('click', calculate);
+
     container.addEventListener('input', function(e){
         btn.disabled = false;  
         if (e.target.classList.contains('inp')){
             e.target.value=e.target.value.replace(/[^0-9]/g,"")
         }
     })
+
 
     this.document.addEventListener('keydown', function(e) {
         if (e.key === '+'){
@@ -23,28 +45,9 @@ window.addEventListener('load', function(){
             oper.value='*'
         } else if (e.key === '/'){
             oper.value='/'
+        } else if (e.key === 'Enter'){
+            calculate()
         } 
     })
-
-    btn.addEventListener('click', function(){
-        let num1 = parseInt(inp1.value);
-        let num2 = parseInt(inp2.value);
-        let result
-
-        if(oper.value==='+'){ 
-            result = num1 + num2
-        } else if (oper.value==='-'){
-            result = num1 - num2
-        } else if (oper.value==='*'){
-            result = num1 * num2
-        } else if (oper.value==='/'){
-            result = num1 / num2
-        }
-        if (isNaN(result)){
-            result='Ошибка'
-        }
-        res.innerHTML = result 
-        btn.disabled = true
-    });
 });
 
